b4555b8ed177daca823c195c9da9ac62d718dcd0
husheng <1175076318@qq.com>
2023-04-07 17:10:54 +0800

    项目初始化

---
+++ heima/src/Demo.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+    public void save(){
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+
+    }
+}
---
+++ heima/src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.save();
+    }
+}
